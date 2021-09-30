25bf6b9b9542b936b16311c009254dd33e03813d
Shellb1 <56370289+Shellb1@users.noreply.github.com>
2021-09-30 18:54:02 -0500

    Setting up database infrastructure.

---
+++ src/main/java/ItemModel.java
@@ -0,0 +1,90 @@
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "GroceryList")
+public class ItemModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "store")
+	private String store;
+	
+	@Column(name = "item")
+	private String item;
+	
+	@Column(name = "quantity")
+	private Integer quantity;
+	
+	@Column(name = "category")
+	private String category;
+
+	public ItemModel(Integer id, String store, String item, Integer quantity, String category) {
+		this.id = id;
+		this.store = store;
+		this.item = item;
+		this.quantity = quantity;
+		this.category = category;
+	}
+	
+	public ItemModel(String store, String item, Integer quantity, String category) {
+		this.store = store;
+		this.item = item;
+		this.quantity = quantity;
+		this.category = category;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getStore() {
+		return store;
+	}
+
+	public void setStore(String store) {
+		this.store = store;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	@Override
+	public String toString() {
+		return "ItemModel [id=" + id + ", store=" + store + ", item=" + item + ", quantity=" + quantity + ", category="
+				+ category + "]";
+	}
+}
---
+++ src/main/java/Util.java
@@ -0,0 +1,107 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ * @since JavaSE-1.8
+ */
+public class Util {
+   static SessionFactory sessionFactory = null;
+
+   public static SessionFactory getSessionFactory() {
+      if (sessionFactory != null) {
+         return sessionFactory;
+      }
+      Configuration configuration = new Configuration().configure();
+      StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+      sessionFactory = configuration.buildSessionFactory(builder.build());
+      return sessionFactory;
+   }
+
+   public static List<ItemModel> getAll() {
+      List<ItemModel> resultList = new ArrayList<ItemModel>();
+
+      Session session = getSessionFactory().openSession();
+      Transaction tx = null;
+
+      try {
+         tx = session.beginTransaction();
+         List<?> items = session.createQuery("FROM GroceryList").list();
+         for (Iterator<?> iterator = items.iterator(); iterator.hasNext();) {
+            ItemModel item = (ItemModel) iterator.next();
+            resultList.add(item);
+         }
+         tx.commit();
+      } catch (HibernateException e) {
+         if (tx != null)
+            tx.rollback();
+         e.printStackTrace();
+      } finally {
+         session.close();
+      }
+      return resultList;
+   }
+
+   public static List<ItemModel> query(String store, String itemName, String category) {
+      List<ItemModel> resultList = new ArrayList<ItemModel>();
+
+      Session session = getSessionFactory().openSession();
+      Transaction tx = null;
+
+      try {
+         tx = session.beginTransaction();
+         List<?> items = session.createQuery("FROM GroceryList").list();
+         for (Iterator<?> iterator = items.iterator(); iterator.hasNext();) {
+            ItemModel item = (ItemModel) iterator.next();
+            if (store != null) {
+            	if (item.getStore().startsWith(store) && !resultList.contains(item)) {
+            		resultList.add(item);
+            	}
+            }
+            if (itemName != null) {
+            	if (item.getItem().startsWith(itemName) && !resultList.contains(item)) {
+            		resultList.add(item);
+            	}
+            }
+            if (category != null) {
+            	if (item.getCategory().startsWith(category) && !resultList.contains(item)) {
+            		resultList.add(item);
+            	}
+            }
+         }
+         tx.commit();
+      } catch (HibernateException e) {
+         if (tx != null)
+            tx.rollback();
+         e.printStackTrace();
+      } finally {
+         session.close();
+      }
+      return resultList;
+   }
+
+   public static void createItem(String store, String item, Integer quantity, String category) {
+      Session session = getSessionFactory().openSession();
+      Transaction tx = null;
+      try {
+         tx = session.beginTransaction();
+         session.save(new ItemModel(store, item, quantity, category));
+         tx.commit();
+      } catch (HibernateException e) {
+         if (tx != null)
+            tx.rollback();
+         e.printStackTrace();
+      } finally {
+         session.close();
+      }
+   }
+}
